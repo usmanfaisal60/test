b15cd3c63dd23e9efff5f4fa7313367bce196323
Usman Faisal <usman.faisal60@gmail.com>
2019-11-16 14:06:56 +0500

    =dsfs

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,7 +62,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'Age' => \App\Http\Middleware\AgeMiddleware::class,
-        'Role' => \App\Http\Middleware\RoleMiddleware::class
     ];
 
     /**
